f76ec94c3db9859962d17f0e207825e457d3592a
Gabriel <gbuzak@gmail.com>
2025-01-26 23:44:00 -0300

    database add4

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from .schema import ProdutosSchema
-from .config import SessionLocal, get_db
+from .config import get_db
 from .model import Produto
 
 router = APIRouter()
